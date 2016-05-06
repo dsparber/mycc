1fb17cef5f85da4cca906b646bd9913be0cee8c4
Daniel Sparber <sparber.daniel@gmail.com>
2016-05-06 10:43:08 +0200

    Updated Tag class

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -4,13 +4,26 @@ namespace MyCryptos
 {
 	public class Tag
 	{
-		private string Name { get; set; }
+		public string Name { get; set; }
 
-		private double Units { get; set; }
+		public double Units { get; set; }
+
+		public bool IsUnlimited {
+			get { return Units < 0; }
+			set {
+				if (value)
+					Units = -1;
+				else
+					Units = 0;
+			}
+		}
+
+
 
 		public Tag (string name)
 		{
 			this.Name = name;
+			this.IsUnlimited = true;
 		}
 	}
 }
