5670b38f86a921220a8d5b5f6eeb751107305753
Daniel Sparber <daniel.sparber@outlook.com>
2016-11-21 20:32:23 +0100

    Layout fix for big devices

--- view/components/ChangingStackLayout.cs
+++ view/components/ChangingStackLayout.cs
@@ -7,12 +7,27 @@ namespace MyCryptos.view.components
         public ChangingStackLayout()
         {
             Spacing = 0;
+            SetChildLayout();
         }
 
         protected override void OnSizeAllocated(double width, double height)
         {
             base.OnSizeAllocated(width, height);
             Orientation = width > height ? StackOrientation.Horizontal : StackOrientation.Vertical;
+        }
+
+        protected override void OnChildAdded(Element child)
+        {
+            base.OnChildAdded(child);
+            SetChildLayout();
+        }
+
+        private void SetChildLayout()
+        {
+            if (Children.Count > 1)
+            {
+                Children[1].HorizontalOptions = LayoutOptions.FillAndExpand;
+            }
         }
     }
 }
