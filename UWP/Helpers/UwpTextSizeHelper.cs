1c1863a545018c785bee27dd67478fc4cfb1fe2a
Daniel Sparber <daniel.sparber@outlook.com>
2016-12-01 21:51:19 +0100

    Updated UWP Project

---
+++ UWP/Helpers/UwpTextSizeHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MyCryptos.helpers;
+using MyCryptos.UWP.Helpers;
+using Xamarin.Forms;
+using Windows.UI.Xaml.Controls;
+
+[assembly: Dependency(typeof(UwpTextSizeHelper))]
+namespace MyCryptos.UWP.Helpers
+{
+    public class UwpTextSizeHelper : TextSizeHelper
+    {
+        public Tuple<double, double> calculateWidth(string text, float? fontsize = null, bool bold = false)
+        {
+            var tb = new TextBlock { Text = text };
+            if (fontsize.HasValue)
+            {
+                tb.FontSize = fontsize.Value;
+            }
+            tb.Measure(new Windows.Foundation.Size(double.PositiveInfinity, double.PositiveInfinity));
+            return Tuple.Create(tb.DesiredSize.Height, tb.DesiredSize.Width);
+        }
+
+    }
+}
---
+++ UWP/data/database/SQLiteConnectionUWP.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Windows.Storage;
+using data.database.interfaces;
+using MyCryptos.UWP.data.database;
+using Xamarin.Forms;
+using SQLite;
+
+[assembly: Dependency(typeof(SqLiteConnectionUwp))]
+namespace MyCryptos.UWP.data.database
+{
+    public class SqLiteConnectionUwp : ISQLiteConnection
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            const string sqliteFilename = "MyCryptos.db";
+            var path = Path.Combine(ApplicationData.Current.LocalFolder.Path, sqliteFilename);
+            var conn = new SQLiteAsyncConnection(path);
+            return conn;
+        }
+    }
+}
+
