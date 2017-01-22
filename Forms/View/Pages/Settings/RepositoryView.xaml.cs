5c70b029cba3360fc79c13c5c231f1d6c7504ece
Daniel Sparber <daniel.sparber@outlook.com>
2017-01-22 09:19:15 +0100

    Bugfix name changed

--- Forms/View/Pages/Settings/RepositoryView.xaml.cs
+++ Forms/View/Pages/Settings/RepositoryView.xaml.cs
@@ -13,194 +13,196 @@ using Xamarin.Forms;
 
 namespace MyCC.Forms.view.pages.settings
 {
-    public partial class RepositoryView
-    {
-        private readonly AccountRepository repository;
+	public partial class RepositoryView
+	{
+		private readonly AccountRepository repository;
 
-        public RepositoryView(AccountRepository repository)
-        {
-            InitializeComponent();
-            this.repository = repository;
+		public RepositoryView(AccountRepository repository)
+		{
+			InitializeComponent();
+			this.repository = repository;
 
-            Header.TitleText = repository is LocalAccountRepository ? I18N.ManuallyAdded : repository.Name;
-            if (repository is AddressAccountRepository)
-            {
-                Header.InfoText = $"{I18N.Source}: {repository.Description}";
-            }
-            else
-            {
-                Header.InfoText = PluralHelper.GetTextCurrencies(repository.ElementsCount);
-                GeneralSection.Remove(AddressEntryCell);
-            }
+			Header.TitleText = repository.Name;
+			if (repository is AddressAccountRepository)
+			{
+				Header.InfoText = $"{I18N.Source}: {repository.Description}";
+				AccountsSection.Title = I18N.Amount;
+			}
+			else
+			{
+				Header.InfoText = PluralHelper.GetTextCurrencies(repository.ElementsCount);
+				GeneralSection.Remove(AddressEntryCell);
+			}
 
-            if (repository is LocalAccountRepository)
-            {
-                TableView.Root.Remove(GeneralSection);
-                ToolbarItems.Remove(EditItem);
-            }
+			if (repository is BittrexAccountRepository)
+			{
+				AccountsSection.Title = I18N.Accounts;
+			}
 
-            RepositoryNameEntryCell.Text = repository.Name;
-            DeleteButtonCell.Tapped += Delete;
+			RepositoryNameEntryCell.Text = repository.Name;
+			DeleteButtonCell.Tapped += Delete;
 
-            TableView.Root.Remove(DeleteSection);
-            ToolbarItems.Remove(SaveItem);
+			TableView.Root.Remove(DeleteSection);
+			ToolbarItems.Remove(SaveItem);
 
-            RepositoryNameEntryCell.Entry.TextChanged += (sender, e) => Header.TitleText = e.NewTextValue;
+			RepositoryNameEntryCell.Entry.TextChanged += (sender, e) => Header.TitleText = e.NewTextValue;
 
-            if (Device.OS == TargetPlatform.Android)
-            {
-                Title = string.Empty;
-            }
+			if (Device.OS == TargetPlatform.Android)
+			{
+				Title = string.Empty;
+			}
 
-            SetView();
+			SetView();
 
-            Messaging.UpdatingAccounts.SubscribeStartedAndFinished(this, () => Device.BeginInvokeOnMainThread(() => Header.IsLoading = true), SetView);
-            Messaging.UpdatingAccountsAndRates.SubscribeStartedAndFinished(this, () => Device.BeginInvokeOnMainThread(() => Header.IsLoading = true), SetView);
-        }
+			Messaging.UpdatingAccounts.SubscribeStartedAndFinished(this, () => Device.BeginInvokeOnMainThread(() => Header.IsLoading = true), SetView);
+			Messaging.UpdatingAccountsAndRates.SubscribeStartedAndFinished(this, () => Device.BeginInvokeOnMainThread(() => Header.IsLoading = true), SetView);
+		}
 
-        private void SetView()
-        {
-            var cells = repository.Elements.OrderBy(e => e.Money.Currency.Code).Select(e =>
-            {
-                var cell = new CustomViewCell { Text = e.Money.ToString(), Detail = e.Money.Currency.Name };
-                if (repository is LocalAccountRepository)
-                {
-                    cell.Image = "more.png";
-                    cell.Detail = e.Name;
-                    cell.Tapped += (sender, eventArgs) => Navigation.PushAsync(new AccountEditView(e, repository as LocalAccountRepository));
-                }
-                return cell;
-            }).ToList();
+		private void SetView()
+		{
+			var cells = repository.Elements.OrderBy(e => e.Money.Currency.Code).Select(e =>
+			{
+				var cell = new CustomViewCell { Text = e.Money.ToString(), Detail = e.Money.Currency.Name };
+				if (repository is LocalAccountRepository)
+				{
+					cell.Image = "more.png";
+					cell.Detail = e.Name;
+					cell.Tapped += (sender, eventArgs) => Navigation.PushAsync(new AccountEditView(e, repository as LocalAccountRepository));
+				}
+				return cell;
+			}).ToList();
 
-            if (cells.Count == 0)
-            {
-                cells.Add(new CustomViewCell
-                {
-                    Text = I18N.NoAccounts
-                });
-            }
-            Device.BeginInvokeOnMainThread(() =>
-            {
-                Header.IsLoading = false;
-                SortHelper.ApplySortOrder(cells, AccountsSection, SortOrder.None, SortDirection.Ascending);
-            });
+			if (cells.Count == 0)
+			{
+				cells.Add(new CustomViewCell
+				{
+					Text = I18N.NoAccounts
+				});
+			}
+			Device.BeginInvokeOnMainThread(() =>
+			{
+				Header.IsLoading = false;
+				SortHelper.ApplySortOrder(cells, AccountsSection, SortOrder.None, SortDirection.Ascending);
+			});
 
-            if (repository is AddressAccountRepository)
-            {
-                Device.BeginInvokeOnMainThread(() =>
-                {
-                    AddressEntryCell.Text = (repository as AddressAccountRepository).Address;
-                    Header.IsLoading = false;
-                });
-            }
-        }
+			if (repository is AddressAccountRepository)
+			{
+				Device.BeginInvokeOnMainThread(() =>
+				{
+					AddressEntryCell.Text = (repository as AddressAccountRepository).Address;
+					Header.IsLoading = false;
+				});
+			}
+		}
 
-        private async void Delete(object sender, EventArgs e)
-        {
-            UnfocusAll();
+		private async void Delete(object sender, EventArgs e)
+		{
+			UnfocusAll();
 
-            Header.LoadingText = I18N.Deleting;
-            Header.IsLoading = true;
-            RepositoryNameEntryCell.IsEditable = false;
+			Header.LoadingText = I18N.Deleting;
+			Header.IsLoading = true;
+			RepositoryNameEntryCell.IsEditable = false;
 
-            await AccountStorage.Instance.Remove(repository);
-            Messaging.UpdatingAccounts.SendFinished();
-            await Navigation.PopAsync();
-        }
+			await AccountStorage.Instance.Remove(repository);
+			Messaging.UpdatingAccounts.SendFinished();
+			await Navigation.PopAsync();
+		}
 
-        private void EditClicked(object sender, EventArgs e)
-        {
-            RepositoryNameEntryCell.IsEditable = true;
-            if (!(repository is LocalAccountRepository))
-            {
-                TableView.Root.Add(DeleteSection);
-            }
-            TableView.Root.Remove(AccountsSection);
+		private void EditClicked(object sender, EventArgs e)
+		{
+			RepositoryNameEntryCell.IsEditable = true;
+			if (!(repository is LocalAccountRepository))
+			{
+				TableView.Root.Add(DeleteSection);
+			}
+			TableView.Root.Remove(AccountsSection);
 
-            Title = I18N.Editing;
-            RepositoryNameEntryCell.IsEditable = true;
-            AddressEntryCell.IsEditable = true;
+			Title = I18N.Editing;
+			RepositoryNameEntryCell.IsEditable = true;
+			AddressEntryCell.IsEditable = true;
 
-            ToolbarItems.Remove(EditItem);
-            ToolbarItems.Add(SaveItem);
-        }
+			ToolbarItems.Remove(EditItem);
+			ToolbarItems.Add(SaveItem);
+		}
 
-        private async void SaveClicked(object sender, EventArgs e)
-        {
-            UnfocusAll();
+		private async void SaveClicked(object sender, EventArgs e)
+		{
+			UnfocusAll();
 
-            SaveItem.Clicked -= SaveClicked;
-            Header.IsLoading = true;
-            RepositoryNameEntryCell.IsEditable = false;
-            AddressEntryCell.IsEditable = false;
+			SaveItem.Clicked -= SaveClicked;
+			Header.IsLoading = true;
+			RepositoryNameEntryCell.IsEditable = false;
+			AddressEntryCell.IsEditable = false;
 
-            Action revertChanges = () => { };
-            var success = true;
-            var changed = false;
+			Action revertChanges = () => { };
+			var success = true;
+			var changed = false;
 
-            repository.Name = RepositoryNameEntryCell.Text ?? string.Empty;
-            if (repository is AddressAccountRepository)
-            {
-                var addressReposiotry = repository as AddressAccountRepository;
-                var oldAddress = addressReposiotry.Address;
-                var addressText = AddressEntryCell.Text ?? string.Empty;
-                if (!addressText.Equals(addressReposiotry.Address))
-                {
-                    addressReposiotry.Address = addressText;
-                    changed = true;
-                }
-                revertChanges = () => addressReposiotry.Address = oldAddress;
-            }
-            else if (repository is BittrexAccountRepository)
-            {
-                // TODO Api Keys
-            }
+			repository.Name = RepositoryNameEntryCell.Text ?? string.Empty;
+			foreach (var a in repository.Elements) a.Name = repository.Name;
 
-            if (changed && repository is OnlineAccountRepository)
-            {
-                var successful = await AccountStorage.AddRepository(repository as OnlineAccountRepository);
-                if (successful)
-                {
-                    await AppTaskHelper.FetchMissingRates();
-                }
-                else
-                {
-                    await DisplayAlert(I18N.Error, I18N.FetchingNoSuccessText, I18N.Ok);
-                    success = false;
+			if (repository is AddressAccountRepository)
+			{
+				var addressReposiotry = repository as AddressAccountRepository;
+				var oldAddress = addressReposiotry.Address;
+				var addressText = AddressEntryCell.Text ?? string.Empty;
+				if (!addressText.Equals(addressReposiotry.Address))
+				{
+					addressReposiotry.Address = addressText;
+					changed = true;
+				}
+				revertChanges = () => addressReposiotry.Address = oldAddress;
+			}
+			else if (repository is BittrexAccountRepository)
+			{
+				// TODO Api Keys
+			}
 
-                    revertChanges();
-                }
-            }
+			if (changed && repository is OnlineAccountRepository)
+			{
+				var successful = await AccountStorage.AddRepository(repository as OnlineAccountRepository);
+				if (successful)
+				{
+					await AppTaskHelper.FetchMissingRates();
+				}
+				else
+				{
+					await DisplayAlert(I18N.Error, I18N.FetchingNoSuccessText, I18N.Ok);
+					success = false;
 
-            if (success)
-            {
+					revertChanges();
+				}
+			}
 
-                await AccountStorage.Instance.Update(repository);
-                Messaging.UpdatingAccounts.SendFinished();
+			if (success)
+			{
 
-                RepositoryNameEntryCell.IsEditable = false;
-                AddressEntryCell.IsEditable = false;
+				await AccountStorage.Instance.Update(repository);
+				Messaging.UpdatingAccounts.SendFinished();
 
-                TableView.Root.Add(AccountsSection);
-                TableView.Root.Remove(DeleteSection);
+				RepositoryNameEntryCell.IsEditable = false;
+				AddressEntryCell.IsEditable = false;
 
-                ToolbarItems.Remove(SaveItem);
-                ToolbarItems.Add(EditItem);
+				TableView.Root.Add(AccountsSection);
+				TableView.Root.Remove(DeleteSection);
 
-                Title = I18N.Accounts;
-            }
-            else
-            {
-                RepositoryNameEntryCell.IsEditable = true;
-                AddressEntryCell.IsEditable = true;
-            }
-            SaveItem.Clicked += SaveClicked;
-            Header.IsLoading = false;
-        }
+				ToolbarItems.Remove(SaveItem);
+				ToolbarItems.Add(EditItem);
 
-        private void UnfocusAll()
-        {
-            RepositoryNameEntryCell.Entry.Unfocus();
-        }
-    }
+				Title = I18N.Accounts;
+			}
+			else
+			{
+				RepositoryNameEntryCell.IsEditable = true;
+				AddressEntryCell.IsEditable = true;
+			}
+			SaveItem.Clicked += SaveClicked;
+			Header.IsLoading = false;
+		}
+
+		private void UnfocusAll()
+		{
+			RepositoryNameEntryCell.Entry.Unfocus();
+		}
+	}
 }
