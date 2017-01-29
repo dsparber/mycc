a62926c1c82ca1e2cc695b88dd9be27f7cbed890
Daniel Sparber <daniel.sparber@outlook.com>
2017-01-29 22:30:34 +0100

    Fixes

--- Core/Account/Database/AccountDatabase.cs
+++ Core/Account/Database/AccountDatabase.cs
@@ -2,30 +2,35 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using MyCC.Core.Abstract.Database;
 using MyCC.Core.Account.Models.Base;
+using MyCC.Core.Settings;
 using SQLite;
 
 namespace MyCC.Core.Account.Database
 {
-    public class AccountDatabase : AbstractDatabase<AccountDbm, FunctionalAccount, int>
-    {
-        public override async Task<IEnumerable<AccountDbm>> GetAllDbObjects()
-        {
-            return await (await Connection).Table<AccountDbm>().ToListAsync();
-        }
+	public class AccountDatabase : AbstractDatabase<AccountDbm, FunctionalAccount, int>
+	{
+		public override async Task<IEnumerable<AccountDbm>> GetAllDbObjects()
+		{
+			return await (await Connection).Table<AccountDbm>().ToListAsync();
+		}
 
-        protected override async Task Create(SQLiteAsyncConnection connection)
-        {
-            await connection.CreateTableAsync<AccountDbm>();
-        }
+		protected override async Task Create(SQLiteAsyncConnection connection)
+		{
+			await connection.CreateTableAsync<AccountDbm>();
+			if (ApplicationSettings.VersionLastLaunch < new Version("0.5.12"))
+			{
+				await connection.ExecuteAsync("DELETE FROM Accounts;");
+			}
+		}
 
-        public override async Task<AccountDbm> GetDbObject(int id)
-        {
-            return await (await Connection).FindAsync<AccountDbm>(p => p.Id == id);
-        }
+		public override async Task<AccountDbm> GetDbObject(int id)
+		{
+			return await (await Connection).FindAsync<AccountDbm>(p => p.Id == id);
+		}
 
-        protected override AccountDbm Resolve(FunctionalAccount element)
-        {
-            return new AccountDbm(element);
-        }
-    }
+		protected override AccountDbm Resolve(FunctionalAccount element)
+		{
+			return new AccountDbm(element);
+		}
+	}
 }
--- Core/Rates/Repositories/BittrexExchangeRateRepository.cs
+++ Core/Rates/Repositories/BittrexExchangeRateRepository.cs
@@ -41,7 +41,7 @@ namespace MyCC.Core.Rates.Repositories
 			var content = await response.Content.ReadAsStringAsync();
 			var resultJson = JObject.Parse(content)[ResultKey];
 
-			var fetchedRates = (from r in resultJson let market = ((string)r[MarketKey]).Split('-') let rate = 1 / decimal.Parse((string)r[RateKey], NumberStyles.Float) select new ExchangeRate(market[0], true, market[1], true, rate) { RepositoryId = TypeId }).ToList();
+			var fetchedRates = (from r in resultJson let market = ((string)r[MarketKey]).Split('-') let rate = 1 / decimal.Parse((string)r[RateKey], NumberStyles.Float, CultureInfo.InvariantCulture) select new ExchangeRate(market[0], true, market[1], true, rate) { RepositoryId = TypeId }).ToList();
 
 			var old = Rates.Except(fetchedRates).ToList();
 
