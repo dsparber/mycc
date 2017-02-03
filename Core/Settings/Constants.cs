999c7cb4c93089b7f7f2a2acacebdc00ed6842da
Daniel Sparber <daniel.sparber@outlook.com>
2017-02-03 13:20:45 +0100

    Version 0.5.21

--- Core/Settings/Constants.cs
+++ Core/Settings/Constants.cs
@@ -2,6 +2,6 @@
 {
 	public static class Constants
 	{
-		public static readonly Version AppVersion = new Version("0.5.20");
+		public static readonly Version AppVersion = new Version("0.5.21");
 	}
 }
