3a60a42b3ec6af5ee975ef6769495de38c24cd0f
Daniel Sparber <daniel.sparber@outlook.com>
2017-02-05 12:52:25 +0100

    Ui fixes

--- Forms/View/Pages/Settings/AboutView.cs
+++ Forms/View/Pages/Settings/AboutView.cs
@@ -7,28 +7,28 @@ using MyCC.Forms.constants;
 
 namespace MyCC.Forms.View.Pages.Settings
 {
-	public class AboutView : ContentPage
-	{
-		HybridWebView _webView;
+    public class AboutView : ContentPage
+    {
+        private readonly HybridWebView _webView;
 
-		public AboutView()
-		{
-			_webView = new HybridWebView { VerticalOptions = LayoutOptions.FillAndExpand, HorizontalOptions = LayoutOptions.FillAndExpand };
+        public AboutView()
+        {
+            _webView = new HybridWebView { VerticalOptions = LayoutOptions.FillAndExpand, HorizontalOptions = LayoutOptions.FillAndExpand };
 
-			var stack = new StackLayout { Spacing = 0, VerticalOptions = LayoutOptions.FillAndExpand, HorizontalOptions = LayoutOptions.FillAndExpand };
-			stack.Children.Add(new HeaderView { TitleText = I18N.AppName, InfoText = $"{I18N.Version} {Constants.AppVersion}" });
-			stack.Children.Add(_webView);
-			Content = stack;
-			BackgroundColor = AppConstants.TableBackgroundColor;
-			Title = I18N.About;
-		}
+            var stack = new ChangingStackLayout { VerticalOptions = LayoutOptions.FillAndExpand, HorizontalOptions = LayoutOptions.FillAndExpand };
+            stack.Children.Add(new HeaderView { TitleText = I18N.AppName, InfoText = $"{I18N.Version} {Constants.AppVersion}" });
+            stack.Children.Add(_webView);
+            Content = stack;
+            BackgroundColor = AppConstants.TableBackgroundColor;
+            Title = I18N.About;
+        }
 
-		protected override void OnAppearing()
-		{
-			base.OnAppearing();
-			_webView.LoadFromContent("Html/about.html");
-			_webView.CallJsFunction("window.history.back");
-		}
-	}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _webView.LoadFromContent("Html/about.html");
+            _webView.CallJsFunction("window.history.back");
+        }
+    }
 }
 
