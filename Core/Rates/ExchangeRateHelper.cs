94aeb3563605d7a81132524c88328901dc1ce419
Daniel Sparber <daniel.sparber@outlook.com>
2017-02-10 15:16:17 +0100

    Added Tasks

--- Core/Rates/ExchangeRateHelper.cs
+++ Core/Rates/ExchangeRateHelper.cs
@@ -6,189 +6,212 @@ using MyCC.Core.Rates.Repositories.Interfaces;
 
 namespace MyCC.Core.Rates
 {
-    public static class ExchangeRateHelper
-    {
+	public static class ExchangeRateHelper
+	{
 
-        public static ExchangeRate GetRate(ExchangeRate rate) => GetRate(new Currency.Model.Currency(rate.ReferenceCurrencyCode, rate.ReferenceCurrencyIsCryptoCurrency), new Currency.Model.Currency(rate.SecondaryCurrencyCode, rate.SecondaryCurrencyIsCryptoCurrency));
+		public static ExchangeRate GetRate(ExchangeRate rate) => GetRate(new Currency.Model.Currency(rate.ReferenceCurrencyCode, rate.ReferenceCurrencyIsCryptoCurrency), new Currency.Model.Currency(rate.SecondaryCurrencyCode, rate.SecondaryCurrencyIsCryptoCurrency));
 
-        public static ExchangeRate GetRate(Currency.Model.Currency currency1, Currency.Model.Currency currency2)
-        {
-            if (string.IsNullOrWhiteSpace(currency1?.Code) ||
-                string.IsNullOrWhiteSpace(currency2?.Code)) throw new ArgumentException();
+		public static ExchangeRate GetRate(Currency.Model.Currency currency1, Currency.Model.Currency currency2)
+		{
+			if (string.IsNullOrWhiteSpace(currency1?.Code) ||
+				string.IsNullOrWhiteSpace(currency2?.Code)) throw new ArgumentException();
 
-            if (currency1.Equals(currency2)) return new ExchangeRate(currency1, currency2, DateTime.Now, 1);
-
-
-            var r1 = GetRateToBtc(currency1);
-            var r2 = GetRateToBtc(currency2);
-
-            if (r1 == null || r2 == null) return null;
-
-            return GetCombinedRate(r1, r2);
-        }
-
-        private static ExchangeRate GetRateToBtc(Currency.Model.Currency currency)
-        {
-            if (currency.Code.Equals("BTC")) return new ExchangeRate(Currency.Model.Currency.Btc, Currency.Model.Currency.Btc, DateTime.Now, 1);
-
-            if (currency.IsCryptoCurrency) return GetStoredRate(currency, Currency.Model.Currency.Btc);
-
-            var rate = currency.Code.Equals("EUR") ? new ExchangeRate(Currency.Model.Currency.Eur, Currency.Model.Currency.Eur, DateTime.Now, 1) : GetStoredRate(currency, Currency.Model.Currency.Eur);
-            var eurBtc = GetStoredRate(Currency.Model.Currency.Eur, Currency.Model.Currency.Btc);
-            return GetCombinedRate(rate, eurBtc);
-        }
-
-        private static ExchangeRate GetStoredRate(Currency.Model.Currency c1, Currency.Model.Currency c2)
-        {
-            var neededRate = new ExchangeRate(c1, c2, DateTime.Now);
-            var rates = ExchangeRatesStorage.Instance.StoredRates.ToList();
-            return rates.Find(c => c?.Equals(neededRate) ?? false) ??
-                    rates.Find(c => c?.Equals(neededRate.Inverse) ?? false)?.Inverse;
-        }
-
-        public static Task FetchMissingRatesFor(IEnumerable<ExchangeRate> rates, Action<double> progressCallback = null)
-        {
-            var missingRates = rates.SelectMany(GetNeededRates).Distinct();
-            return FetchMissingRates(missingRates, progressCallback);
-        }
-
-        private static async Task FetchMissingRates(IEnumerable<ExchangeRate> missingRates, Action<double> progressCallback = null)
-        {
-            var storedRates = ExchangeRatesStorage.Instance.StoredRates.ToList();
-            var neededToFetch = missingRates.Where(r => !(storedRates.Contains(r) || storedRates.Contains(r.Inverse))).ToList();
-
-            var fetchedFixerIo = false;
-            var fetchedBtce = false;
-
-            var progress = .0;
-            foreach (var r in neededToFetch)
-            {
-                progressCallback?.Invoke(progress / neededToFetch.Count); progress += 1;
-
-                if (ExchangeRatesStorage.FixerIo.IsAvailable(r) || ExchangeRatesStorage.FixerIo.IsAvailable(r.Inverse))
-                {
-                    if (fetchedFixerIo) continue;
-
-                    fetchedFixerIo = true;
-                    await ExchangeRatesStorage.FixerIo.FetchRates();
-                }
-                else if (ExchangeRatesStorage.Btce.IsAvailable(r) || ExchangeRatesStorage.Btce.IsAvailable(r.Inverse))
-                {
-                    if (fetchedBtce) continue;
-
-                    fetchedBtce = true;
-                    await ExchangeRatesStorage.Btce.FetchRates();
-                }
-                else
-                {
-                    var supportedRepo =
-                        ExchangeRatesStorage.Instance.Repositories.FirstOrDefault(repo => repo.IsAvailable(r)) ??
-                        ExchangeRatesStorage.Instance.Repositories.FirstOrDefault(repo => repo.IsAvailable(r.Inverse));
-
-                    if (supportedRepo == null) continue;
-
-                    if (supportedRepo is IMultipleRatesRepository)
-                    {
-                        await ((IMultipleRatesRepository)supportedRepo).FetchRates();
-                    }
-                    else
-                    {
-                        await ((ISingleRateRepository)supportedRepo).FetchRate(supportedRepo.IsAvailable(r) ? r : r.Inverse);
-                    }
-                }
-            }
-            progressCallback?.Invoke(1);
-        }
-
-        public static async Task UpdateRates(IEnumerable<ExchangeRate> rates, Action<double> progressCallback = null)
-        {
-            var updatedRepositories = new List<int>();
-
-            var ratesToUpdate = rates.SelectMany(GetNeededRates).ToList();
-
-            var progress = .0;
-            foreach (var r in ratesToUpdate)
-            {
-                progressCallback?.Invoke(progress / ratesToUpdate.Count); progress += 1;
-
-                var supportedRepo = ExchangeRatesStorage.Instance.Repositories.FirstOrDefault(repo => repo.IsAvailable(r));
-                if (supportedRepo == null) continue;
-
-                var multiRepo = supportedRepo as IMultipleRatesRepository;
-                if (multiRepo != null)
-                {
-                    if (updatedRepositories.Contains(multiRepo.TypeId)) continue;
-
-                    updatedRepositories.Add(multiRepo.TypeId);
-                    await multiRepo.FetchRates();
-                }
-                else
-                {
-                    await ((ISingleRateRepository)supportedRepo).FetchRate(r);
-                }
-            }
-            progressCallback?.Invoke(1);
-        }
-
-        public static IEnumerable<ExchangeRate> GetNeededRates(ExchangeRate rate)
-        {
-            if (string.IsNullOrWhiteSpace(rate?.ReferenceCurrencyCode) ||
-                string.IsNullOrWhiteSpace(rate.SecondaryCurrencyCode)) throw new ArgumentNullException();
-
-            if (rate.ReferenceCurrencyCode.Equals(rate.SecondaryCurrencyCode)) return new List<ExchangeRate>();
+			if (currency1.Equals(currency2)) return new ExchangeRate(currency1, currency2, DateTime.Now, 1);
 
 
-            var r1 = GetNeededRatesToBtc(new Currency.Model.Currency(rate.ReferenceCurrencyCode, rate.ReferenceCurrencyIsCryptoCurrency));
-            var r2 = GetNeededRatesToBtc(new Currency.Model.Currency(rate.SecondaryCurrencyCode, rate.SecondaryCurrencyIsCryptoCurrency));
+			var r1 = GetRateToBtc(currency1);
+			var r2 = GetRateToBtc(currency2);
 
-            return r1.Concat(r2);
-        }
+			if (r1 == null || r2 == null) return null;
 
-        private static IEnumerable<ExchangeRate> GetNeededRatesToBtc(Currency.Model.Currency currency)
-        {
-            if (currency.Code.Equals("BTC")) return new List<ExchangeRate>();
+			return GetCombinedRate(r1, r2);
+		}
 
-            if (currency.IsCryptoCurrency) return new List<ExchangeRate> { new ExchangeRate(currency, Currency.Model.Currency.Btc) };
+		private static ExchangeRate GetRateToBtc(Currency.Model.Currency currency)
+		{
+			if (currency.Code.Equals("BTC")) return new ExchangeRate(Currency.Model.Currency.Btc, Currency.Model.Currency.Btc, DateTime.Now, 1);
 
-            return new List<ExchangeRate> {
-                new ExchangeRate(currency, Currency.Model.Currency.Eur),
-                new ExchangeRate(Currency.Model.Currency.Eur, Currency.Model.Currency.Btc)
-            }.Where(r => r.ReferenceCurrencyCode != r.SecondaryCurrencyCode);
-        }
+			if (currency.IsCryptoCurrency) return GetStoredRate(currency, Currency.Model.Currency.Btc);
+
+			var rate = currency.Code.Equals("EUR") ? new ExchangeRate(Currency.Model.Currency.Eur, Currency.Model.Currency.Eur, DateTime.Now, 1) : GetStoredRate(currency, Currency.Model.Currency.Eur);
+			var eurBtc = GetStoredRate(Currency.Model.Currency.Eur, Currency.Model.Currency.Btc);
+			return GetCombinedRate(rate, eurBtc);
+		}
+
+		private static ExchangeRate GetStoredRate(Currency.Model.Currency c1, Currency.Model.Currency c2)
+		{
+			var neededRate = new ExchangeRate(c1, c2, DateTime.Now);
+			var rates = ExchangeRatesStorage.Instance.StoredRates.ToList();
+			return rates.Find(c => c?.Equals(neededRate) ?? false) ??
+					rates.Find(c => c?.Equals(neededRate.Inverse) ?? false)?.Inverse;
+		}
+
+		public static Task FetchMissingRatesFor(IEnumerable<ExchangeRate> rates, Action<double> progressCallback = null)
+		{
+			var missingRates = rates.SelectMany(GetNeededRates).Distinct();
+			return FetchMissingRates(missingRates, progressCallback);
+		}
+
+		private static async Task FetchMissingRates(IEnumerable<ExchangeRate> missingRates, Action<double> progressCallback = null)
+		{
+			var storedRates = ExchangeRatesStorage.Instance.StoredRates.ToList();
+			var neededToFetch = missingRates.Where(r => !(storedRates.Contains(r) || storedRates.Contains(r.Inverse))).ToList();
+
+			var fetchedFixerIo = false;
+			var fetchedBtce = false;
+
+			var progress = .0;
+			foreach (var r in neededToFetch)
+			{
+				progressCallback?.Invoke(progress / neededToFetch.Count); progress += 1;
+
+				if (ExchangeRatesStorage.FixerIo.IsAvailable(r) || ExchangeRatesStorage.FixerIo.IsAvailable(r.Inverse))
+				{
+					if (fetchedFixerIo) continue;
+
+					fetchedFixerIo = true;
+					await ExchangeRatesStorage.FixerIo.FetchRates();
+				}
+				else if (ExchangeRatesStorage.Btce.IsAvailable(r) || ExchangeRatesStorage.Btce.IsAvailable(r.Inverse))
+				{
+					if (fetchedBtce) continue;
+
+					fetchedBtce = true;
+					await ExchangeRatesStorage.Btce.FetchRates();
+				}
+				else
+				{
+					var supportedRepo =
+						ExchangeRatesStorage.Instance.Repositories.FirstOrDefault(repo => repo.IsAvailable(r)) ??
+						ExchangeRatesStorage.Instance.Repositories.FirstOrDefault(repo => repo.IsAvailable(r.Inverse));
+
+					if (supportedRepo == null) continue;
+
+					if (supportedRepo is IMultipleRatesRepository)
+					{
+						await ((IMultipleRatesRepository)supportedRepo).FetchRates();
+					}
+					else
+					{
+						await ((ISingleRateRepository)supportedRepo).FetchRate(supportedRepo.IsAvailable(r) ? r : r.Inverse);
+					}
+				}
+			}
+			progressCallback?.Invoke(1);
+		}
+
+		public static async Task UpdateRates(IEnumerable<ExchangeRate> rates, Action<double> progressCallback = null)
+		{
+			var updatedRepositories = new List<int>();
+
+			var ratesToUpdate = rates.SelectMany(GetNeededRates).ToList();
+
+			var progress = .0;
+			foreach (var r in ratesToUpdate)
+			{
+				progressCallback?.Invoke(progress / ratesToUpdate.Count); progress += 1;
+
+				var supportedRepo = ExchangeRatesStorage.Instance.Repositories.FirstOrDefault(repo => repo.IsAvailable(r));
+				if (supportedRepo == null) continue;
+
+				var multiRepo = supportedRepo as IMultipleRatesRepository;
+				if (multiRepo != null)
+				{
+					if (updatedRepositories.Contains(multiRepo.TypeId)) continue;
+
+					updatedRepositories.Add(multiRepo.TypeId);
+					await multiRepo.FetchRates();
+				}
+				else
+				{
+					await ((ISingleRateRepository)supportedRepo).FetchRate(r);
+				}
+			}
+			progressCallback?.Invoke(1);
+		}
+
+		public static async Task FetchDollarBitcoinRates(Action<double> progressCallback = null)
+		{
+			var repos = ExchangeRatesStorage.Instance.Repositories.Where(r => r.RatesType == RateRepositoryType.CryptoToFiat).ToList();
+
+			var progress = .0;
+			foreach (var r in repos)
+			{
+
+				var multiRepo = r as IMultipleRatesRepository;
+				if (multiRepo != null)
+				{
+					await multiRepo.FetchRates();
+				}
+				else
+				{
+					await ((ISingleRateRepository)r).FetchRate(new ExchangeRate(Currency.Model.Currency.Btc, Currency.Model.Currency.Usd));
+				}
+
+				progress += 1;
+				progressCallback?.Invoke(progress / repos.Count);
+			}
+		}
+
+		public static IEnumerable<ExchangeRate> GetNeededRates(ExchangeRate rate)
+		{
+			if (string.IsNullOrWhiteSpace(rate?.ReferenceCurrencyCode) ||
+				string.IsNullOrWhiteSpace(rate.SecondaryCurrencyCode)) throw new ArgumentNullException();
+
+			if (rate.ReferenceCurrencyCode.Equals(rate.SecondaryCurrencyCode)) return new List<ExchangeRate>();
+
+
+			var r1 = GetNeededRatesToBtc(new Currency.Model.Currency(rate.ReferenceCurrencyCode, rate.ReferenceCurrencyIsCryptoCurrency));
+			var r2 = GetNeededRatesToBtc(new Currency.Model.Currency(rate.SecondaryCurrencyCode, rate.SecondaryCurrencyIsCryptoCurrency));
+
+			return r1.Concat(r2);
+		}
+
+		private static IEnumerable<ExchangeRate> GetNeededRatesToBtc(Currency.Model.Currency currency)
+		{
+			if (currency.Code.Equals("BTC")) return new List<ExchangeRate>();
+
+			if (currency.IsCryptoCurrency) return new List<ExchangeRate> { new ExchangeRate(currency, Currency.Model.Currency.Btc) };
+
+			return new List<ExchangeRate> {
+				new ExchangeRate(currency, Currency.Model.Currency.Eur),
+				new ExchangeRate(Currency.Model.Currency.Eur, Currency.Model.Currency.Btc)
+			}.Where(r => r.ReferenceCurrencyCode != r.SecondaryCurrencyCode);
+		}
 
 
 
-        private static ExchangeRate GetCombinedRate(ExchangeRate rate1, ExchangeRate rate2)
-        {
-            if (rate1 == null || rate2 == null) return null;
+		private static ExchangeRate GetCombinedRate(ExchangeRate rate1, ExchangeRate rate2)
+		{
+			if (rate1 == null || rate2 == null) return null;
 
-            var r = new ExchangeRate(DifferentCurrency(rate1, rate2), DifferentCurrency(rate2, rate1), new[] { rate1.LastUpdate, rate2.LastUpdate }.Min());
+			var r = new ExchangeRate(DifferentCurrency(rate1, rate2), DifferentCurrency(rate2, rate1), new[] { rate1.LastUpdate, rate2.LastUpdate }.Min());
 
-            var r1 = InvertIfNeeded(rate1, CommonCurrency(rate1, rate2));
-            var r2 = InvertIfNeeded(rate2, CommonCurrency(rate2, rate1));
+			var r1 = InvertIfNeeded(rate1, CommonCurrency(rate1, rate2));
+			var r2 = InvertIfNeeded(rate2, CommonCurrency(rate2, rate1));
 
-            r.Rate = r2.Rate / r1.Rate;
+			r.Rate = r2.Rate / r1.Rate;
 
-            return r;
-        }
+			return r;
+		}
 
-        private static Currency.Model.Currency DifferentCurrency(ExchangeRate r1, ExchangeRate r2)
-        {
-            if (CommonCurrency(r1, r2) == null) return null;
+		private static Currency.Model.Currency DifferentCurrency(ExchangeRate r1, ExchangeRate r2)
+		{
+			if (CommonCurrency(r1, r2) == null) return null;
 
-            return r2.Contains(r1.ReferenceCurrencyCode, r1.ReferenceCurrencyIsCryptoCurrency) ?
-                     new Currency.Model.Currency(r1.SecondaryCurrencyCode, r1.SecondaryCurrencyIsCryptoCurrency) :
-                     new Currency.Model.Currency(r1.ReferenceCurrencyCode, r1.SecondaryCurrencyIsCryptoCurrency);
-        }
+			return r2.Contains(r1.ReferenceCurrencyCode, r1.ReferenceCurrencyIsCryptoCurrency) ?
+					 new Currency.Model.Currency(r1.SecondaryCurrencyCode, r1.SecondaryCurrencyIsCryptoCurrency) :
+					 new Currency.Model.Currency(r1.ReferenceCurrencyCode, r1.SecondaryCurrencyIsCryptoCurrency);
+		}
 
-        private static string CommonCurrency(ExchangeRate r1, ExchangeRate r2)
-        {
-            return r2.Contains(r1.ReferenceCurrencyCode, r1.ReferenceCurrencyIsCryptoCurrency) ? r1.ReferenceCurrencyCode : r2.Contains(r1.SecondaryCurrencyCode, r1.SecondaryCurrencyIsCryptoCurrency) ? r1.SecondaryCurrencyCode : null;
-        }
+		private static string CommonCurrency(ExchangeRate r1, ExchangeRate r2)
+		{
+			return r2.Contains(r1.ReferenceCurrencyCode, r1.ReferenceCurrencyIsCryptoCurrency) ? r1.ReferenceCurrencyCode : r2.Contains(r1.SecondaryCurrencyCode, r1.SecondaryCurrencyIsCryptoCurrency) ? r1.SecondaryCurrencyCode : null;
+		}
 
-        private static ExchangeRate InvertIfNeeded(ExchangeRate rate, string currencyCode)
-        {
-            return currencyCode.Equals(rate.ReferenceCurrencyCode) ? rate : rate.Inverse;
-        }
-    }
+		private static ExchangeRate InvertIfNeeded(ExchangeRate rate, string currencyCode)
+		{
+			return currencyCode.Equals(rate.ReferenceCurrencyCode) ? rate : rate.Inverse;
+		}
+	}
 }
--- Forms/Tasks/AppTaskHelper.cs
+++ Forms/Tasks/AppTaskHelper.cs
@@ -12,79 +12,84 @@ using MyCC.Forms.View.Overlays;
 
 namespace MyCC.Forms.Tasks
 {
-    public static class AppTaskHelper
-    {
-        public static async Task FetchMissingRates(List<ExchangeRate> neededRates, double progressSpan = 1, double progressStart = 0)
-        {
-            if (neededRates.Count > 0)
-            {
-                await ApplicationTasks.FetchMissingRates(neededRates, Messaging.FetchMissingRates.SendStarted, Messaging.FetchMissingRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(progressStart + progressSpan * d));
-            }
-        }
+	public static class AppTaskHelper
+	{
+		public static async Task FetchMissingRates(List<ExchangeRate> neededRates, double progressSpan = 1, double progressStart = 0)
+		{
+			if (neededRates.Count > 0)
+			{
+				await ApplicationTasks.FetchMissingRates(neededRates, Messaging.FetchMissingRates.SendStarted, Messaging.FetchMissingRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(progressStart + progressSpan * d));
+			}
+		}
 
-        public static async Task FetchMissingRates(double progressSpan = 1, double progressStart = 0)
-        {
-            var neededRates = ApplicationSettings.WatchedCurrencies
-                                    .Concat(ApplicationSettings.AllReferenceCurrencies)
-                                    .SelectMany(c => ApplicationSettings.AllReferenceCurrencies.Select(r => new ExchangeRate(r, c)))
-                                    .Distinct()
-                                    .Select(r => ExchangeRateHelper.GetRate(r) ?? r)
-                                    .Where(r => r.Rate == null)
-                                    .Concat(AccountStorage.NeededRates).Distinct().ToList();
-            await FetchMissingRates(neededRates, progressSpan, progressStart);
-        }
+		public static async Task FetchMissingRates(double progressSpan = 1, double progressStart = 0)
+		{
+			var neededRates = ApplicationSettings.WatchedCurrencies
+									.Concat(ApplicationSettings.AllReferenceCurrencies)
+									.SelectMany(c => ApplicationSettings.AllReferenceCurrencies.Select(r => new ExchangeRate(r, c)))
+									.Distinct()
+									.Select(r => ExchangeRateHelper.GetRate(r) ?? r)
+									.Where(r => r.Rate == null)
+									.Concat(AccountStorage.NeededRates).Distinct().ToList();
+			await FetchMissingRates(neededRates, progressSpan, progressStart);
+		}
 
-        public static async Task FetchBalancesAndRates()
-        {
-            await ApplicationTasks.FetchAccounts(Messaging.UpdatingAccounts.SendStarted, Messaging.UpdatingAccounts.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(0.5 * d));
-            await ApplicationTasks.FetchRates(Messaging.UpdatingRates.SendStarted, Messaging.UpdatingRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(0.5 + d * 0.4));
-            await FetchMissingRates(AccountStorage.NeededRates.ToList(), 0.1, 0.9);
-            Messaging.Progress.Send(1);
+		public static async Task FetchBalancesAndRates()
+		{
+			await ApplicationTasks.FetchAccounts(Messaging.UpdatingAccounts.SendStarted, Messaging.UpdatingAccounts.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(0.5 * d));
+			await ApplicationTasks.FetchRates(Messaging.UpdatingRates.SendStarted, Messaging.UpdatingRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(0.5 + d * 0.4));
+			await FetchMissingRates(AccountStorage.NeededRates.ToList(), 0.1, 0.9);
+			Messaging.Progress.Send(1);
 
-        }
+		}
 
-        public static async Task FetchBalanceAndRates(FunctionalAccount account)
-        {
-            Messaging.Progress.Send(0.1);
-            await ApplicationTasks.FetchBalance(account, Messaging.UpdatingAccountsAndRates.SendStarted, Messaging.UpdatingAccountsAndRates.SendFinished, ErrorOverlay.Display);
-            await ApplicationTasks.FetchRates(account, Messaging.UpdatingAccountsAndRates.SendStarted, Messaging.UpdatingAccountsAndRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(0.3 + 0.4 * d));
-            await FetchMissingRates(AccountStorage.NeededRatesFor(account), 0.3, 0.7);
-            Messaging.Progress.Send(1);
-        }
+		public static async Task FetchBalanceAndRates(FunctionalAccount account)
+		{
+			Messaging.Progress.Send(0.1);
+			await ApplicationTasks.FetchBalance(account, Messaging.UpdatingAccountsAndRates.SendStarted, Messaging.UpdatingAccountsAndRates.SendFinished, ErrorOverlay.Display);
+			await ApplicationTasks.FetchRates(account, Messaging.UpdatingAccountsAndRates.SendStarted, Messaging.UpdatingAccountsAndRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(0.3 + 0.4 * d));
+			await FetchMissingRates(AccountStorage.NeededRatesFor(account), 0.3, 0.7);
+			Messaging.Progress.Send(1);
+		}
 
-        public static async Task FetchBalanceAndRates(Currency currency)
-        {
-            Messaging.Progress.Send(0.1);
-            await ApplicationTasks.FetchBalance(currency, Messaging.UpdatingAccountsAndRates.SendStarted, Messaging.UpdatingAccountsAndRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(d * 0.4));
-            await ApplicationTasks.FetchRates(currency, Messaging.UpdatingAccountsAndRates.SendStarted, Messaging.UpdatingAccountsAndRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(0.4 + 0.3 * d));
-            await FetchMissingRates(AccountStorage.NeededRatesFor(currency), 0.3, 0.7);
-            Messaging.Progress.Send(1);
-        }
+		public static async Task FetchBalanceAndRates(Currency currency)
+		{
+			Messaging.Progress.Send(0.1);
+			await ApplicationTasks.FetchBalance(currency, Messaging.UpdatingAccountsAndRates.SendStarted, Messaging.UpdatingAccountsAndRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(d * 0.4));
+			await ApplicationTasks.FetchRates(currency, Messaging.UpdatingAccountsAndRates.SendStarted, Messaging.UpdatingAccountsAndRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(0.4 + 0.3 * d));
+			await FetchMissingRates(AccountStorage.NeededRatesFor(currency), 0.3, 0.7);
+			Messaging.Progress.Send(1);
+		}
 
 
-        public static async Task UpdateRates()
-        {
-            await ApplicationTasks.FetchRates(Messaging.UpdatingRates.SendStarted, Messaging.UpdatingRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(d * 0.8));
-            await FetchMissingRates(0.2, 0.8);
-            Messaging.Progress.Send(1);
-        }
+		public static async Task UpdateRates()
+		{
+			await ApplicationTasks.FetchRates(Messaging.UpdatingRates.SendStarted, Messaging.UpdatingRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(d * 0.8));
+			await FetchMissingRates(0.2, 0.8);
+			Messaging.Progress.Send(1);
+		}
 
-        public static async Task FetchCoinDetails(Currency currency)
-        {
-            Messaging.Progress.Send(0.2);
-            await ApplicationTasks.FetchCoinInfo(currency, Messaging.FetchingCoinInfo.SendStarted, Messaging.FetchingCoinInfo.SendFinished, ErrorOverlay.Display);
-            Messaging.Progress.Send(0.4);
-            await ApplicationTasks.FetchRates(currency, Messaging.UpdatingAccountsAndRates.SendStarted, Messaging.UpdatingAccountsAndRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(0.4 + d * 0.6));
-            Messaging.Progress.Send(1);
+		public static async Task FetchBtcUsdRates()
+		{
+			await ApplicationTasks.FetchRates(Messaging.UpdatingRates.SendStarted, Messaging.UpdatingRates.SendFinished, ErrorOverlay.Display, Messaging.Progress.Send);
+		}
 
-        }
+		public static async Task FetchCoinDetails(Currency currency)
+		{
+			Messaging.Progress.Send(0.2);
+			await ApplicationTasks.FetchCoinInfo(currency, Messaging.FetchingCoinInfo.SendStarted, Messaging.FetchingCoinInfo.SendFinished, ErrorOverlay.Display);
+			Messaging.Progress.Send(0.4);
+			await ApplicationTasks.FetchRates(currency, Messaging.UpdatingAccountsAndRates.SendStarted, Messaging.UpdatingAccountsAndRates.SendFinished, ErrorOverlay.Display, d => Messaging.Progress.Send(0.4 + d * 0.6));
+			Messaging.Progress.Send(1);
 
-        public static async Task FetchCoinInfo(Currency currency)
-        {
-            Messaging.Progress.Send(0.3);
-            await ApplicationTasks.FetchCoinInfo(currency, Messaging.FetchingCoinInfo.SendStarted, Messaging.FetchingCoinInfo.SendFinished, ErrorOverlay.Display);
-            Messaging.Progress.Send(1);
+		}
 
-        }
-    }
+		public static async Task FetchCoinInfo(Currency currency)
+		{
+			Messaging.Progress.Send(0.3);
+			await ApplicationTasks.FetchCoinInfo(currency, Messaging.FetchingCoinInfo.SendStarted, Messaging.FetchingCoinInfo.SendFinished, ErrorOverlay.Display);
+			Messaging.Progress.Send(1);
+
+		}
+	}
 }
