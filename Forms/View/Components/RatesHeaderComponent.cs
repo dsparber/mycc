2e45b445affe5aa4146fe42d15e858f97530a833
Daniel Sparber <daniel.sparber@outlook.com>
2017-02-17 18:03:13 +0100

    Bugfix

--- Forms/View/Components/RatesHeaderComponent.cs
+++ Forms/View/Components/RatesHeaderComponent.cs
@@ -12,86 +12,87 @@ using Xamarin.Forms;
 
 namespace MyCC.Forms.View.Components
 {
-    public class RatesHeaderComponent : HeaderView
-    {
-        private readonly Currency _currency;
-        private readonly List<string> _infoTexts;
+	public class RatesHeaderComponent : HeaderView
+	{
+		private readonly Currency _currency;
+		private readonly List<string> _infoTexts;
 
-        private static readonly List<RatesHeaderComponent> Instances = new List<RatesHeaderComponent>();
-        private static int _currentInfoText = 1;
+		private static readonly List<RatesHeaderComponent> Instances = new List<RatesHeaderComponent>();
+		private static int _currentInfoText = 1;
 
-        public RatesHeaderComponent(Currency currency) : base(true)
-        {
-            _currency = currency;
+		public RatesHeaderComponent(Currency currency) : base(true)
+		{
+			_currency = currency;
 
-            _infoTexts = new List<string> { string.Empty, string.Empty, string.Empty };
+			_infoTexts = new List<string> { string.Empty, string.Empty, string.Empty };
 
-            var recognizer = new TapGestureRecognizer();
-            recognizer.Tapped += (sender, e) => SetInfoText(+1);
-            GestureRecognizers.Add(recognizer);
+			var recognizer = new TapGestureRecognizer();
+			recognizer.Tapped += (sender, e) => SetInfoText(+1);
+			GestureRecognizers.Add(recognizer);
 
-            AddSubscriber();
-            UpdateView();
+			AddSubscriber();
+			UpdateView();
 
-            Instances.Add(this);
-        }
+			Instances.Add(this);
+		}
 
-        private void UpdateView()
-        {
-            _infoTexts[0] = _currency.Name;
-            _infoTexts[1] = string.Join(" / ", ApplicationSettings.MainCurrencies
-                            .Where(c => !c.Equals(_currency))
-                            .Select(c => new Money(ExchangeRateHelper.GetRate(Currency.Btc, c)?.Rate ?? 0, c)
-                            .ToStringTwoDigits(ApplicationSettings.RoundMoney)));
-            _infoTexts[2] = ApplicationSettings.WatchedCurrencies
-                            .Concat(ApplicationSettings.AllReferenceCurrencies)
-                            .Concat(AccountStorage.UsedCurrencies)
-                            .Select(e => new ExchangeRate(_currency, e))
-                            .SelectMany(ExchangeRateHelper.GetNeededRates)
-                            .Distinct()
-                            .Select(e => ExchangeRateHelper.GetRate(e)?.LastUpdate ?? DateTime.Now).Min().LastUpdateString();
+		private void UpdateView()
+		{
+			_infoTexts[0] = _currency.Name;
+			_infoTexts[1] = string.Join(" / ", ApplicationSettings.MainCurrencies
+							.Where(c => !c.Equals(_currency))
+							.Select(c => new Money(ExchangeRateHelper.GetRate(Currency.Btc, c)?.Rate ?? 0, c)
+							.ToStringTwoDigits(ApplicationSettings.RoundMoney)));
+			var lastUpdate = ApplicationSettings.WatchedCurrencies
+							.Concat(ApplicationSettings.AllReferenceCurrencies)
+							.Concat(AccountStorage.UsedCurrencies)
+							.Select(e => new ExchangeRate(_currency, e))
+							.SelectMany(ExchangeRateHelper.GetNeededRates)
+							.Distinct()
+							.Select(e => ExchangeRateHelper.GetRate(e)?.LastUpdate ?? DateTime.Now);
+			_infoTexts[2] = lastUpdate.Any() ? lastUpdate.Min().LastUpdateString() : DateTime.Now.LastUpdateString();
 
-            Device.BeginInvokeOnMainThread(() =>
-            {
+			Device.BeginInvokeOnMainThread(() =>
+			{
 
-                TitleText = Sum.ToStringTwoDigits(ApplicationSettings.RoundMoney);
-                SetInfoText();
-            });
-        }
+				TitleText = Sum.ToStringTwoDigits(ApplicationSettings.RoundMoney);
+				SetInfoText();
+			});
+		}
 
-        private Money Sum => new Money(ExchangeRateHelper.GetRate(Currency.Btc, _currency)?.Rate ?? 0, _currency);
+		private Money Sum => new Money(ExchangeRateHelper.GetRate(Currency.Btc, _currency)?.Rate ?? 0, _currency);
 
-        private void AddSubscriber()
-        {
-            Messaging.ReferenceCurrency.SubscribeValueChanged(this, UpdateView);
-            Messaging.RoundNumbers.SubscribeValueChanged(this, UpdateView);
+		private void AddSubscriber()
+		{
+			Messaging.ReferenceCurrency.SubscribeValueChanged(this, UpdateView);
+			Messaging.RoundNumbers.SubscribeValueChanged(this, UpdateView);
 
-            Messaging.FetchMissingRates.SubscribeFinished(this, UpdateView);
-            Messaging.UpdatingAccountsAndRates.SubscribeFinished(this, UpdateView);
-            Messaging.UpdatingAccounts.SubscribeFinished(this, UpdateView);
-            Messaging.UpdatingRates.SubscribeFinished(this, UpdateView);
-            Messaging.Loading.SubscribeFinished(this, UpdateView);
-        }
+			Messaging.FetchMissingRates.SubscribeFinished(this, UpdateView);
+			Messaging.UpdatingAccountsAndRates.SubscribeFinished(this, UpdateView);
+			Messaging.UpdatingAccounts.SubscribeFinished(this, UpdateView);
+			Messaging.UpdatingRates.SubscribeFinished(this, UpdateView);
+			Messaging.Loading.SubscribeFinished(this, UpdateView);
+		}
 
-        private void SetInfoText(int increment = 0, bool updateOthers = true)
-        {
-            _currentInfoText = (_currentInfoText + increment) % _infoTexts.Count;
+		private void SetInfoText(int increment = 0, bool updateOthers = true)
+		{
+			_currentInfoText = (_currentInfoText + increment) % _infoTexts.Count;
 
-            if (_infoTexts == null || _infoTexts.Count < _currentInfoText) return;
+			if (_infoTexts == null || _infoTexts.Count < _currentInfoText) return;
 
-            var text = _infoTexts[_currentInfoText];
-            if (string.IsNullOrEmpty(text?.Trim()))
-            {
-                text = _infoTexts[(_currentInfoText + 1) % _infoTexts.Count];
-            }
-            InfoText = text;
+			var text = _infoTexts[_currentInfoText];
+			if (string.IsNullOrEmpty(text?.Trim()))
+			{
+				text = _infoTexts[(_currentInfoText + 1) % _infoTexts.Count];
+			}
+			InfoText = text;
 
-            if (!updateOthers) return;
+			if (!updateOthers) return;
 
-            foreach (var i in Instances)
-            {
-                i.SetInfoText(0, false);
-            }
-        }
-    }
+			foreach (var i in Instances)
+			{
+				i.SetInfoText(0, false);
+			}
+		}
+	}
 }
