a09ed0f57dd5e2b37554e4dd237cf666d3e29809
Daniel Sparber <daniel.sparber@outlook.com>
2017-03-10 17:03:02 +0100

    Refined add view

--- Core/Account/Storage/AccountStorage.cs
+++ Core/Account/Storage/AccountStorage.cs
@@ -12,63 +12,52 @@ using MyCC.Core.Settings;
 
 namespace MyCC.Core.Account.Storage
 {
-    public class AccountStorage : AbstractDatabaseStorage<AccountRepositoryDbm, AccountRepository, AccountDbm, FunctionalAccount, int>
-    {
-        private AccountStorage() : base(new AccountRepositoryDatabase()) { }
+	public class AccountStorage : AbstractDatabaseStorage<AccountRepositoryDbm, AccountRepository, AccountDbm, FunctionalAccount, int>
+	{
+		private AccountStorage() : base(new AccountRepositoryDatabase()) { }
 
-        protected override async Task OnFirstLaunch()
-        {
-            var localRepository = new LocalAccountRepository(default(int), I18N.LocalStorage);
-            await Add(localRepository);
-        }
+		protected override async Task OnFirstLaunch()
+		{
+			var localRepository = new LocalAccountRepository(default(int), I18N.LocalStorage);
+			await Add(localRepository);
+		}
 
-        public override AccountRepository LocalRepository => Repositories.OfType<LocalAccountRepository>().FirstOrDefault();
+		public override AccountRepository LocalRepository => Repositories.OfType<LocalAccountRepository>().FirstOrDefault();
 
-        public static readonly AccountStorage Instance = new AccountStorage();
+		public static readonly AccountStorage Instance = new AccountStorage();
 
-        public static IEnumerable<Currency.Model.Currency> UsedCurrencies => Instance.AllElements.Select(a => a?.Money?.Currency).Distinct().Where(e => e != null).ToList();
-        public static IEnumerable<IGrouping<Currency.Model.Currency, Models.Base.Account>> AccountsGroupedByCurrency => Instance.AllElements.GroupBy(a => a.Money.Currency);
-        public static List<FunctionalAccount> AccountsWithCurrency(Currency.Model.Currency currency) => Instance.AllElements.Where(a => a.Money.Currency.Equals(currency)).ToList();
+		public static IEnumerable<Currency.Model.Currency> UsedCurrencies => Instance.AllElements.Select(a => a?.Money?.Currency).Distinct().Where(e => e != null).ToList();
+		public static IEnumerable<IGrouping<Currency.Model.Currency, Models.Base.Account>> AccountsGroupedByCurrency => Instance.AllElements.GroupBy(a => a.Money.Currency);
+		public static List<FunctionalAccount> AccountsWithCurrency(Currency.Model.Currency currency) => Instance.AllElements.Where(a => a.Money.Currency.Equals(currency)).ToList();
 
 
-        public static List<ExchangeRate> NeededRates => UsedCurrencies.Distinct()
-                                       .SelectMany(c => ApplicationSettings.AllReferenceCurrencies.Select(cref => new ExchangeRate(c, cref)))
-                                       .Select(e => ExchangeRateHelper.GetRate(e) ?? e)
-                                       .Where(r => r?.Rate == null)
-                                       .ToList();
+		public static List<ExchangeRate> NeededRates => UsedCurrencies.Distinct()
+									   .SelectMany(c => ApplicationSettings.AllReferenceCurrencies.Select(cref => new ExchangeRate(c, cref)))
+									   .Select(e => ExchangeRateHelper.GetRate(e) ?? e)
+									   .Where(r => r?.Rate == null)
+									   .ToList();
 
-        public static List<ExchangeRate> NeededRatesFor(Currency.Model.Currency accountCurrency) => ApplicationSettings.AllReferenceCurrencies
-                                       .Select(c => new ExchangeRate(accountCurrency, c))
-                                       .Select(e => ExchangeRateHelper.GetRate(e) ?? e)
-                                       .Where(r => r.Rate == null)
-                                       .ToList();
+		public static List<ExchangeRate> NeededRatesFor(Currency.Model.Currency accountCurrency) => ApplicationSettings.AllReferenceCurrencies
+									   .Select(c => new ExchangeRate(accountCurrency, c))
+									   .Select(e => ExchangeRateHelper.GetRate(e) ?? e)
+									   .Where(r => r.Rate == null)
+									   .ToList();
 
-        public static List<ExchangeRate> NeededRatesFor(FunctionalAccount account) => NeededRatesFor(account.Money.Currency);
+		public static List<ExchangeRate> NeededRatesFor(FunctionalAccount account) => NeededRatesFor(account.Money.Currency);
 
-        public static AccountRepository RepositoryOf(FunctionalAccount account) => Instance.Repositories.Find(r => r.Id == account.ParentId);
+		public static AccountRepository RepositoryOf(FunctionalAccount account) => Instance.Repositories.Find(r => r.Id == account.ParentId);
 
-        public static Task Update(FunctionalAccount account) => RepositoryOf(account).Update(account);
+		public static Task Update(FunctionalAccount account) => RepositoryOf(account).Update(account);
 
-        public static IEnumerable<FunctionalAccount> EnabledAccounts => Instance.AllElements.Where(a => a.IsEnabled);
+		public static IEnumerable<FunctionalAccount> EnabledAccounts => Instance.AllElements.Where(a => a.IsEnabled);
 
-        public static int CurrenciesForGraph => AccountsGroupedByCurrency
-            .Select(e => e.Select(a =>
-            {
-                var rate = new ExchangeRate(e.Key, ApplicationSettings.BaseCurrency);
-                rate = ExchangeRateHelper.GetRate(rate) ?? rate;
+		public static int CurrenciesForGraph => AccountsGroupedByCurrency
+			.Select(e => e.Select(a =>
+			{
+				var rate = new ExchangeRate(e.Key, ApplicationSettings.BaseCurrency);
+				rate = ExchangeRateHelper.GetRate(rate) ?? rate;
 
-                return a.IsEnabled ? a.Money.Amount * rate.Rate ?? 0 : 0;
-            }).Sum()).Count(v => v > 0);
-
-        public static async Task<bool> AddRepository(OnlineAccountRepository repository)
-        {
-            var success = await repository.Test();
-            if (!success) return false;
-
-            await Instance.Add(repository);
-            await Instance.FetchOnline();
-            return true;
-        }
-
-    }
+				return a.IsEnabled ? a.Money.Amount * rate.Rate ?? 0 : 0;
+			}).Sum()).Count(v => v > 0);
+	}
 }
--- Forms/View/Overlays/AddSourceView.xaml.cs
+++ Forms/View/Overlays/AddSourceView.xaml.cs
@@ -15,157 +15,159 @@ using Xamarin.Forms;
 
 namespace MyCC.Forms.View.Overlays
 {
-    public partial class AddSourceView
-    {
-        private AddSourceSubview _specificAddView;
+	public partial class AddSourceView
+	{
+		private AddSourceSubview _specificAddView;
 
-        public AddSourceView(bool local = false)
-        {
-            InitializeComponent();
-            Header.TitleText = I18N.NewSource;
-            Header.LoadingText = I18N.Testing;
+		public AddSourceView(bool local = false)
+		{
+			InitializeComponent();
+			Header.TitleText = I18N.NewSource;
+			Header.LoadingText = I18N.Testing;
 
-            if (Device.OS == TargetPlatform.Android)
-            {
-                ToolbarItems.Remove(CancelItem);
-            }
+			if (Device.OS == TargetPlatform.Android)
+			{
+				ToolbarItems.Remove(CancelItem);
+			}
 
-            var addViews = new List<AddSourceSubview>
-            {
-                new AddAddressSubview(Navigation, NameEntryCell.Entry),
-                new AddBittrexSubview(),
-                new AddLocalAccountSubview(Navigation)
-            };
+			var addViews = new List<AddSourceSubview>
+			{
+				new AddAddressSubview(Navigation, NameEntryCell.Entry),
+				new AddBittrexSubview(),
+				new AddLocalAccountSubview(Navigation)
+			};
 
-            _specificAddView = addViews[local ? 2 : 0];
-            TableViewComponent.Root.Clear();
-            foreach (var s in _specificAddView.InputSections)
-            {
-                TableViewComponent.Root.Add(s);
-            }
-            TableViewComponent.Root.Add(NameSection);
+			_specificAddView = addViews[local ? 2 : 0];
+			TableViewComponent.Root.Clear();
+			foreach (var s in _specificAddView.InputSections)
+			{
+				TableViewComponent.Root.Add(s);
+			}
+			TableViewComponent.Root.Add(NameSection);
 
-            SegmentedControl.BackgroundColor = AppConstants.TableBackgroundColor;
-            SegmentedControl.Tabs = addViews.Select(v => v.Description).ToList();
-            SegmentedControl.SelectedIndex = local ? 2 : 0;
-            SegmentedControl.SelectionChanged = index =>
-            {
-                _specificAddView = addViews[index];
-                NameEntryCell.Placeholder = _specificAddView.DefaultName;
-                var txt = NameEntryCell.Text?.Trim();
-                Header.InfoText = string.Empty.Equals(txt) || txt == null ? _specificAddView.DefaultName : txt;
+			SegmentedControl.BackgroundColor = AppConstants.TableBackgroundColor;
+			SegmentedControl.Tabs = addViews.Select(v => v.Description).ToList();
+			SegmentedControl.SelectedIndex = local ? 2 : 0;
+			SegmentedControl.SelectionChanged = index =>
+			{
+				_specificAddView = addViews[index];
+				NameEntryCell.Placeholder = _specificAddView.DefaultName;
+				var txt = NameEntryCell.Text?.Trim();
+				Header.InfoText = string.Empty.Equals(txt) || txt == null ? _specificAddView.DefaultName : txt;
 
-                TableViewComponent.Root.Clear();
-                foreach (var s in _specificAddView.InputSections)
-                {
-                    TableViewComponent.Root.Add(s);
-                }
-                TableViewComponent.Root.Add(NameSection);
-            };
+				TableViewComponent.Root.Clear();
+				foreach (var s in _specificAddView.InputSections)
+				{
+					TableViewComponent.Root.Add(s);
+				}
+				TableViewComponent.Root.Add(NameSection);
+			};
 
-            Header.InfoText = _specificAddView.DefaultName;
-            NameEntryCell.Placeholder = _specificAddView.DefaultName;
-            NameEntryCell.Entry.TextChanged += (sender, e) => Header.InfoText = e.NewTextValue.Length != 0 ? e.NewTextValue : _specificAddView.DefaultName;
-        }
+			Header.InfoText = _specificAddView.DefaultName;
+			NameEntryCell.Placeholder = _specificAddView.DefaultName;
+			NameEntryCell.Entry.TextChanged += (sender, e) => Header.InfoText = e.NewTextValue.Length != 0 ? e.NewTextValue : _specificAddView.DefaultName;
+		}
 
-        private void Cancel(object sender, EventArgs e)
-        {
-            UnfocusAll();
-            Navigation.PopOrPopModal();
-        }
+		private void Cancel(object sender, EventArgs e)
+		{
+			UnfocusAll();
+			Navigation.PopOrPopModal();
+		}
 
-        private async void Save(object sender, EventArgs e)
-        {
-            try
-            {
-                UnfocusAll();
-                Header.IsLoading = true;
+		private async void Save(object sender, EventArgs e)
+		{
+			try
+			{
+				UnfocusAll();
+				Header.IsLoading = true;
 
-                NameEntryCell.IsEditable = false;
-                _specificAddView.Enabled = false;
+				NameEntryCell.IsEditable = false;
+				_specificAddView.Enabled = false;
 
-                var nameText = (NameEntryCell.Text ?? string.Empty).Trim();
-                var name = nameText.Equals(string.Empty) ? _specificAddView.DefaultName : nameText;
+				var nameText = (NameEntryCell.Text ?? string.Empty).Trim();
+				var name = nameText.Equals(string.Empty) ? _specificAddView.DefaultName : nameText;
 
-                var addView = _specificAddView as AddRepositorySubview;
-                if (addView != null)
-                {
-                    addView.Enabled = false;
-                    var repository = addView.GetRepository(name);
+				var addView = _specificAddView as AddRepositorySubview;
+				if (addView != null)
+				{
+					addView.Enabled = false;
+					var repository = addView.GetRepository(name);
 
-                    if (repository == null)
-                    {
-                        await DisplayAlert(I18N.Error, I18N.VerifyInput, I18N.Cancel);
-                    }
-                    else if (AccountStorage.Instance.RepositoriesOfType(repository.GetType()).Any(r => r.Data.Equals(repository.Data)))
-                    {
-                        await DisplayAlert(I18N.Error, I18N.RepositoryAlreadyAdded, I18N.Cancel);
-                        await Navigation.PopOrPopModal();
-                    }
-                    else
-                    {
+					if (repository == null)
+					{
+						await DisplayAlert(I18N.Error, I18N.VerifyInput, I18N.Cancel);
+					}
+					else if (AccountStorage.Instance.RepositoriesOfType(repository.GetType()).Any(r => r.Data.Equals(repository.Data)))
+					{
+						await DisplayAlert(I18N.Error, I18N.RepositoryAlreadyAdded, I18N.Cancel);
+						await Navigation.PopOrPopModal();
+					}
+					else
+					{
 
-                        var success = await AccountStorage.AddRepository(repository);
-                        if (success)
-                        {
-                            Header.LoadingText = I18N.Fetching;
-                            Messaging.UpdatingAccounts.SendFinished();
-                            await AppTaskHelper.FetchMissingRates();
-                            await Navigation.PopOrPopModal();
+						var success = await repository.Test();
+						if (success)
+						{
+							Header.LoadingText = I18N.Fetching;
+							await AccountStorage.Instance.Add(repository);
+							await repository.FetchOnline();
+							Messaging.UpdatingAccounts.SendFinished();
+							await AppTaskHelper.FetchMissingRates();
+							await Navigation.PopOrPopModal();
 
-                        }
-                        else
-                        {
-                            Header.IsLoading = false;
-                            await DisplayAlert(I18N.Error, I18N.FetchingNoSuccessText, I18N.Ok);
-                        }
-                    }
-                    Header.IsLoading = false;
+						}
+						else
+						{
+							Header.IsLoading = false;
+							await DisplayAlert(I18N.Error, I18N.FetchingNoSuccessText, I18N.Ok);
+						}
+					}
+					Header.IsLoading = false;
 
-                    NameEntryCell.IsEditable = true;
-                    _specificAddView.Enabled = true;
-                    addView.Enabled = true;
-                }
-                else if (_specificAddView is AddAccountSubview)
-                {
-                    var account = ((AddAccountSubview)_specificAddView).GetAccount(name);
+					NameEntryCell.IsEditable = true;
+					_specificAddView.Enabled = true;
+					addView.Enabled = true;
+				}
+				else if (_specificAddView is AddAccountSubview)
+				{
+					var account = ((AddAccountSubview)_specificAddView).GetAccount(name);
 
-                    if (account != null)
-                    {
+					if (account != null)
+					{
 
-                        await AccountStorage.Instance.LocalRepository.Add(account);
-                        Messaging.UpdatingAccounts.SendFinished();
+						await AccountStorage.Instance.LocalRepository.Add(account);
+						Messaging.UpdatingAccounts.SendFinished();
 
-                        var referenceCurrencies = ApplicationSettings.AllReferenceCurrencies.ToList();
-                        var neededRates = referenceCurrencies.Select(c => new ExchangeRate(account.Money.Currency, c)).ToList();
+						var referenceCurrencies = ApplicationSettings.AllReferenceCurrencies.ToList();
+						var neededRates = referenceCurrencies.Select(c => new ExchangeRate(account.Money.Currency, c)).ToList();
 
-                        if (neededRates.Count > 0)
-                        {
-                            await AppTaskHelper.FetchMissingRates();
-                        }
-                        await Navigation.PopOrPopModal();
+						if (neededRates.Count > 0)
+						{
+							await AppTaskHelper.FetchMissingRates();
+						}
+						await Navigation.PopOrPopModal();
 
-                    }
-                    else
-                    {
-                        Header.IsLoading = false;
-                        await DisplayAlert(I18N.Error, I18N.VerifyInput, I18N.Ok);
+					}
+					else
+					{
+						Header.IsLoading = false;
+						await DisplayAlert(I18N.Error, I18N.VerifyInput, I18N.Ok);
 
-                        NameEntryCell.IsEditable = true;
-                        _specificAddView.Enabled = true;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
-        }
+						NameEntryCell.IsEditable = true;
+						_specificAddView.Enabled = true;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+		}
 
-        private void UnfocusAll()
-        {
-            NameEntryCell.Entry.Unfocus();
-            _specificAddView.Unfocus();
-        }
-    }
+		private void UnfocusAll()
+		{
+			NameEntryCell.Entry.Unfocus();
+			_specificAddView.Unfocus();
+		}
+	}
 }
