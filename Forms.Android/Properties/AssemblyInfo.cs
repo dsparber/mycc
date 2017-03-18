e8cfd94b2a01865d1d1965173c293cfc75775d40
Daniel Sparber <daniel.sparber@outlook.com>
2017-03-18 14:00:44 +0100

    Build 0.5.44

--- Forms.Android/Properties/AssemblyInfo.cs
+++ Forms.Android/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.5.43")]
+[assembly: AssemblyVersion("0.5.44")]
 
 #if DEBUG
 [assembly: Application(Debuggable = true)]
