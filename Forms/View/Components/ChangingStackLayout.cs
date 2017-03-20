60ec826016337faf79eb701afa48dcb5166b867c
Daniel Sparber <daniel.sparber@outlook.com>
2017-03-20 12:55:32 +0100

    FIxes

--- Forms/View/Components/ChangingStackLayout.cs
+++ Forms/View/Components/ChangingStackLayout.cs
@@ -4,40 +4,39 @@ using Xamarin.Forms;
 
 namespace MyCC.Forms.View.Components
 {
-    public class ChangingStackLayout : StackLayout
-    {
-        public ChangingStackLayout()
-        {
-            Spacing = 0;
-            HorizontalOptions = LayoutOptions.FillAndExpand;
-            VerticalOptions = LayoutOptions.FillAndExpand;
-            BackgroundColor = AppConstants.TableBackgroundColor;
-            InputTransparent = true;
-            SetChildLayout();
-        }
+	public class ChangingStackLayout : StackLayout
+	{
+		public ChangingStackLayout()
+		{
+			Spacing = 0;
+			HorizontalOptions = LayoutOptions.FillAndExpand;
+			VerticalOptions = LayoutOptions.FillAndExpand;
+			BackgroundColor = AppConstants.TableBackgroundColor;
+			SetChildLayout();
+		}
 
-        protected override void OnSizeAllocated(double width, double height)
-        {
-            base.OnSizeAllocated(width, height);
-            var orientation = width > height ? StackOrientation.Horizontal : StackOrientation.Vertical;
-            if (orientation == Orientation) return;
+		protected override void OnSizeAllocated(double width, double height)
+		{
+			base.OnSizeAllocated(width, height);
+			var orientation = width > height ? StackOrientation.Horizontal : StackOrientation.Vertical;
+			if (orientation == Orientation) return;
 
-            Orientation = orientation;
-            Messaging.Layout.SendValueChanged();
-        }
+			Orientation = orientation;
+			Messaging.Layout.SendValueChanged();
+		}
 
-        protected override void OnChildAdded(Element child)
-        {
-            base.OnChildAdded(child);
-            SetChildLayout();
-        }
+		protected override void OnChildAdded(Element child)
+		{
+			base.OnChildAdded(child);
+			SetChildLayout();
+		}
 
-        private void SetChildLayout()
-        {
-            if (Children.Count > 1)
-            {
-                Children[1].HorizontalOptions = LayoutOptions.FillAndExpand;
-            }
-        }
-    }
+		private void SetChildLayout()
+		{
+			if (Children.Count > 1)
+			{
+				Children[1].HorizontalOptions = LayoutOptions.FillAndExpand;
+			}
+		}
+	}
 }
--- Forms/View/Pages/Info/CurrencyGroupedInfoView.cs
+++ Forms/View/Pages/Info/CurrencyGroupedInfoView.cs
@@ -16,127 +16,128 @@ using Xamarin.Forms;
 
 namespace MyCC.Forms.View.Pages.Info
 {
-    public class CurrencyGroupedInfoView : ContentPage
-    {
-        public CurrencyGroupedInfoView()
-        {
-            Title = I18N.AvailableCurrencies;
+	public class CurrencyGroupedInfoView : ContentPage
+	{
+		public CurrencyGroupedInfoView()
+		{
+			Title = I18N.AvailableCurrencies;
 
-            var allCurrencies = CurrencyStorage.Instance.AllElements.ToList();
-            var fiatCurrencies = allCurrencies.Where(c => !c.IsCryptoCurrency).ToList();
-            var cryptoCurrencies = allCurrencies.Where(c => c.IsCryptoCurrency).ToList();
+			var allCurrencies = CurrencyStorage.Instance.AllElements.ToList();
+			var fiatCurrencies = allCurrencies.Where(c => !c.IsCryptoCurrency).ToList();
+			var cryptoCurrencies = allCurrencies.Where(c => c.IsCryptoCurrency).ToList();
 
-            var bittrexCurrencies = CurrencyStorage.CurrenciesOf<BittrexCurrencyRepository>().ToList();
-            var addressCurrencies = AddressAccountRepository.AllSupportedCurrencies.ToList();
-            var balanceCurrencies = bittrexCurrencies.Concat(addressCurrencies).Distinct().ToList();
+			var bittrexCurrencies = CurrencyStorage.CurrenciesOf<BittrexCurrencyRepository>().ToList();
+			var addressCurrencies = AddressAccountRepository.AllSupportedCurrencies.ToList();
+			var balanceCurrencies = bittrexCurrencies.Concat(addressCurrencies).Distinct().ToList();
 
-            var coinInfos = CoinInfoStorage.SupportetCurrencies.ToList();
-            var cryptoRates = CurrencyStorage.CurrenciesOf<BittrexCurrencyRepository>()
-                .Concat(CurrencyStorage.CurrenciesOf<CryptonatorCurrencyRepository>())
-                .Concat(CurrencyStorage.CurrenciesOf<BtceCurrencyRepository>())
-                .Distinct().ToList();
-            var fiatRates = ExchangeRatesStorage.FixerIo.Rates.Select(r => CurrencyStorage.Instance.AllElements.Find(x => Equals(x?.Code, r?.SecondaryCurrencyCode) && x?.IsCryptoCurrency == false)).Distinct().ToList();
-            var rates = cryptoRates.Concat(fiatRates).Distinct().ToList();
+			var coinInfos = CoinInfoStorage.SupportetCurrencies.ToList();
+			var cryptoRates = CurrencyStorage.CurrenciesOf<BittrexCurrencyRepository>()
+				.Concat(CurrencyStorage.CurrenciesOf<CryptonatorCurrencyRepository>())
+				.Concat(CurrencyStorage.CurrenciesOf<BtceCurrencyRepository>())
+				.Distinct().ToList();
+			var fiatRates = ExchangeRatesStorage.FixerIo.Rates.Select(r => CurrencyStorage.Instance.AllElements.Find(x => Equals(x?.Code, r?.SecondaryCurrencyCode) && x?.IsCryptoCurrency == false)).Distinct().ToList();
+			var rates = cryptoRates.Concat(fiatRates).Distinct().ToList();
 
-            var allCurrenciesCell = new CustomViewCell
-            {
-                Text = PluralHelper.GetTextCurrencies(allCurrencies.Count),
-                Image = "more",
-            };
-            allCurrenciesCell.Tapped += OpenFor(allCurrencies, I18N.AllCurrencies);
+			var allCurrenciesCell = new CustomViewCell
+			{
+				Text = PluralHelper.GetTextCurrencies(allCurrencies.Count),
+				Image = "more",
+			};
+			allCurrenciesCell.Tapped += OpenFor(allCurrencies, I18N.AllCurrencies);
 
-            var fiatCurrenciesCell = new CustomViewCell
-            {
-                Text = $"{fiatCurrencies.Count} {I18N.FiatCurrencies}",
-                Image = "more",
-            };
-            fiatCurrenciesCell.Tapped += OpenFor(fiatCurrencies, I18N.FiatCurrencies);
+			var fiatCurrenciesCell = new CustomViewCell
+			{
+				Text = $"{fiatCurrencies.Count} {I18N.FiatCurrencies}",
+				Image = "more",
+			};
+			fiatCurrenciesCell.Tapped += OpenFor(fiatCurrencies, I18N.FiatCurrencies);
 
-            var cryptoCurrenciesCell = new CustomViewCell
-            {
-                Text = $"{cryptoCurrencies.Count} {I18N.Cryptocurrencies}",
-                Image = "more",
-            };
-            cryptoCurrenciesCell.Tapped += OpenFor(cryptoCurrencies, I18N.Cryptocurrencies);
+			var cryptoCurrenciesCell = new CustomViewCell
+			{
+				Text = $"{cryptoCurrencies.Count} {I18N.Cryptocurrencies}",
+				Image = "more",
+			};
+			cryptoCurrenciesCell.Tapped += OpenFor(cryptoCurrencies, I18N.Cryptocurrencies);
 
-            var balanceCell = new CustomViewCell
-            {
-                Text = I18N.BalanceAvailable,
-                Detail = PluralHelper.GetTextCurrencies(balanceCurrencies.Count),
-                Image = "more",
-            };
-            balanceCell.Tapped += OpenFor(balanceCurrencies, I18N.BalanceAvailable);
+			var balanceCell = new CustomViewCell
+			{
+				Text = I18N.BalanceAvailable,
+				Detail = PluralHelper.GetTextCurrencies(balanceCurrencies.Count),
+				Image = "more",
+			};
+			balanceCell.Tapped += OpenFor(balanceCurrencies, I18N.BalanceAvailable);
 
-            var addressCell = new CustomViewCell
-            {
-                Text = I18N.BalanceAvailableAddress,
-                Detail = PluralHelper.GetTextCurrencies(addressCurrencies.Count),
-                Image = "more",
-            };
-            addressCell.Tapped += OpenFor(addressCurrencies, I18N.BalanceAvailableAddress);
+			var addressCell = new CustomViewCell
+			{
+				Text = I18N.BalanceAvailableAddress,
+				Detail = PluralHelper.GetTextCurrencies(addressCurrencies.Count),
+				Image = "more",
+			};
+			addressCell.Tapped += OpenFor(addressCurrencies, I18N.BalanceAvailableAddress);
 
-            var bittrexCell = new CustomViewCell
-            {
-                Text = I18N.BalanceAvailableBittrex,
-                Detail = PluralHelper.GetTextCurrencies(bittrexCurrencies.Count),
-                Image = "more",
-            };
-            bittrexCell.Tapped += OpenFor(bittrexCurrencies, I18N.BalanceAvailableBittrex);
+			var bittrexCell = new CustomViewCell
+			{
+				Text = I18N.BalanceAvailableBittrex,
+				Detail = PluralHelper.GetTextCurrencies(bittrexCurrencies.Count),
+				Image = "more",
+			};
+			bittrexCell.Tapped += OpenFor(bittrexCurrencies, I18N.BalanceAvailableBittrex);
 
-            var ratesCell = new CustomViewCell
-            {
-                Text = I18N.Rates,
-                Detail = PluralHelper.GetTextCurrencies(rates.Count),
-                Image = "more",
-            };
-            ratesCell.Tapped += OpenFor(rates, I18N.Rates);
+			var ratesCell = new CustomViewCell
+			{
+				Text = I18N.Rates,
+				Detail = PluralHelper.GetTextCurrencies(rates.Count),
+				Image = "more",
+			};
+			ratesCell.Tapped += OpenFor(rates, I18N.Rates);
 
-            var infosCell = new CustomViewCell
-            {
-                Text = I18N.Info,
-                Detail = PluralHelper.GetTextCurrencies(coinInfos.Count),
-                Image = "more",
-            };
-            infosCell.Tapped += OpenFor(coinInfos, I18N.Info);
+			var infosCell = new CustomViewCell
+			{
+				Text = I18N.Info,
+				Detail = PluralHelper.GetTextCurrencies(coinInfos.Count),
+				Image = "more",
+			};
+			infosCell.Tapped += OpenFor(coinInfos, I18N.Info);
 
 
-            Content = new ChangingStackLayout
-            {
-                Children =
-                {
-                    new HeaderView
-                    {
-                        TitleText = I18N.AppName,
-                        InfoText = PluralHelper.GetTextCurrencies(allCurrencies.Count)
-                    },
-                    new TableView()
-                    {
-                        Root =
-                        {
-                            new TableSection(I18N.General)
-                            {
-                                allCurrenciesCell,
-                                fiatCurrenciesCell,
-                                cryptoCurrenciesCell
-                            },
-                            new TableSection(I18N.Balance)
-                            {
-                                balanceCell,
-                                addressCell,
-                                bittrexCell
-                            },
-                            new TableSection(I18N.Further)
-                            {
-                                ratesCell,
-                                infosCell
-                            }
-                        }
-                    }
-                }
-            };
-        }
+			Content = new ChangingStackLayout
+			{
+				Children =
+				{
+					new HeaderView
+					{
+						TitleText = I18N.AppName,
+						InfoText = PluralHelper.GetTextCurrencies(allCurrencies.Count)
+					},
+					new TableView()
+					{
+						Intent = TableIntent.Form,
+						Root =
+						{
+							new TableSection(I18N.General)
+							{
+								allCurrenciesCell,
+								fiatCurrenciesCell,
+								cryptoCurrenciesCell
+							},
+							new TableSection(I18N.Balance)
+							{
+								balanceCell,
+								addressCell,
+								bittrexCell
+							},
+							new TableSection(I18N.Further)
+							{
+								ratesCell,
+								infosCell
+							}
+						}
+					}
+				}
+			};
+		}
 
-        private EventHandler OpenFor(IEnumerable<Currency> currencies, string title) => (sender, e) => Navigation.PushAsync(new CurrencyOverlay(currencies, title, false, true));
+		private EventHandler OpenFor(IEnumerable<Currency> currencies, string title) => (sender, e) => Navigation.PushAsync(new CurrencyOverlay(currencies, title, false, true));
 
-    }
+	}
 }
