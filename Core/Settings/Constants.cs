e3971862566938d262d2cd389dc0974f1ed4894f
Daniel Sparber <daniel.sparber@outlook.com>
2017-03-23 19:16:05 +0100

    Build 0.5.46

--- Core/Settings/Constants.cs
+++ Core/Settings/Constants.cs
@@ -2,6 +2,6 @@
 {
     public static class Constants
     {
-        public static readonly Version AppVersion = new Version("0.5.45");
+        public static readonly Version AppVersion = new Version("0.5.46");
     }
 }
