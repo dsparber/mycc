0c2598c1b2fd62c50ca7d2fe58ec04f9fbbd825b
Daniel Sparber <daniel.sparber@outlook.com>
2017-04-11 13:11:40 +0200

    iOS Fix

--- Forms/View/Components/Table/ReferenceCurrenciesView.cs
+++ Forms/View/Components/Table/ReferenceCurrenciesView.cs
@@ -180,5 +180,11 @@ namespace MyCC.Forms.View.Components.Table
             }
 
         }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            UpdateView();
+        }
     }
 }
