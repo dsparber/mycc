ad481634b2032810d51a8461de955bc958e1be76
Daniel Sparber <daniel.sparber@outlook.com>
2017-06-11 21:50:08 +0200

    Added slovak, edited about.html

--- Forms/Constants/AppConstants.cs
+++ Forms/Constants/AppConstants.cs
@@ -24,6 +24,6 @@ namespace MyCC.Forms.Constants
 
         public const int LabelWidth = 100;
 
-        public static readonly CoreVersion AppVersion = new CoreVersion(1, 0, 3);
+        public static readonly CoreVersion AppVersion = new CoreVersion(1, 1);
     }
 }
